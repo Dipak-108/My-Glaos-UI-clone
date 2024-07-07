84b8ec510da007c584b2de4e4a424cc9f718819c
Dipak Dahal <dipakdahalconnects@gmail.com>
2024-07-07 09:04:51 +0545

    responsive navbar using resize

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,50 @@
+const hamburgerBtn=document.querySelector("#hamburger_id");
+const navbarUl=document.querySelector(".navbar_ul");
 
-
-function toggleNav(){
-   let toggleVar= document.getElementById("navbar_ul");
-   console.log(toggleVar);
-   console.log(toggleVar.style.display);
-
-   if(toggleVar.style.display=='none'){
-    toggleVar.style.display='flex'
-   }
-   else{
-    toggleVar.style.display='none'
-   }
-   console.log(window.innerWidth);
+hamburgerBtn.addEventListener("click",function (){
+if(navbarUl.classList.contains("hamburger_active")){
+   navbarUl.classList.remove("hamburger_active");
+   
 }
+
+else{
+   navbarUl.classList.add("hamburger_active");
+}
+
+
+})
+
+
+window.addEventListener("resize", (event) => {
+   if(window.screen.width >= 768){
+      navbarUl.classList.remove("hamburger_active");
+   }
+   console.log("rezizing "+window.screen.width);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function toggleNav(){
+//    let toggleVar= document.getElementById("navbar_ul");
+//    console.log(toggleVar);
+//    console.log(toggleVar.style.display);
+
+//    if(toggleVar.style.display=='none'){
+//     toggleVar.style.display='flex'
+//    }
+//    else{
+//     toggleVar.style.display='none'
+//    }
+//    console.log(window.innerWidth);
+// }
 
